8ca6037ce7ca03f1be5d355b6f1b007c00727e5e
Kamil Wachala <kamil.wachala@dpr.co.uk>
2022-08-29 12:06:45 +0200

    Initial structure

---
+++ test/Kamo.Core.Messanger.WinUiTests/Program.cs
@@ -0,0 +1,15 @@
+namespace Kamo.Core.Messanger.WinUiTests
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new TestFrom());
+        }
+    }
+}
---
+++ test/Kamo.Core.Messanger.WinUiTests/TestFrom.cs
@@ -0,0 +1,10 @@
+namespace Kamo.Core.Messanger.WinUiTests
+{
+    public partial class TestFrom : Form
+    {
+        public TestFrom()
+        {
+            InitializeComponent();
+        }
+    }
+}
